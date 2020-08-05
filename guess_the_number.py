b3c4fe4681e3808816be5c9f8c465cb2e295e7c0
Jouhou <robinnymoen@gmail.com>
2020-08-05 23:36:45 +0300

    Modifed the game so that it lists how many guesses the player have left and to only decrese the ammount of guesses left if the player enters a valid number while guessing.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,9 +1,10 @@
 from random import randint
 
 # Sets the initial number variable that the user will try and guess
-# while also setting the win condition to false
 number = randint(1, 20)
-win_condition = False
+sel_diff = None
+# Variable to keep the game loop running
+game_loop = True
 
 # Sets the difficulty parameters
 # The harder the difficulty the less guesses the user have
@@ -13,28 +14,63 @@ difficulty = {
     "hard" : 5
 }
 
-print("Hello and welcome to my guess the number game!!!")
-sel_diff = input("Select your difficulty, Easy, Medium or Hard")
-diff = difficulty.get(sel_diff.lower(), 5)
+# Keeps the game loop running as long as the game_loop variable is true
+while game_loop == True:
 
-for i in range(diff):
-    guess = int(input("Guess a number between 1 and 20: "))
-    if guess == number:
-        print("That's correct, you did it!")
-        win_condition = True
-        break
-    elif guess > 20 or guess < 1:
-        print("I said guess a number between 1 and 20 -_-")
-    elif guess > number:
-        print("Your guess is too high!")
-    elif guess < number:
-        print("Your guess is too low!")
+    # Sets the win_condition variable to false, if true the user has won the game
+    win_condition = False
+
+    print("Hello and welcome to my guess the number game!!!")
+    while True:
+        sel_diff = input("Select your difficulty, Easy, Medium or Hard ")
+        if sel_diff.lower() in ["easy", "medium", "hard"]:
+            break
+        else:
+            print("Please enter one of the listed difficulties.")
+
+    diff = difficulty.get(sel_diff.lower())
+    count = diff
+
+    while count > 0:
+        print(f"You have {count} guesses left")
+        guess = input("Guess a number between 1 and 20: ")
+        # Checks if the guess is a valid integer
+        try:
+            guess == int(guess)
+        except ValueError:
+            print("Please enter a valid number")
+            continue
+
+        guess = int(guess)
+
+        if guess == number:
+            print("That's correct, you did it!")
+            win_condition = True
+            break
+        elif guess > 20 or guess < 1:
+            print("I said guess a number between 1 and 20 -_-")
+        elif guess > number:
+            print("Your guess is too high!")
+            count = count - 1
+        elif guess < number:
+            print("Your guess is too low!")
+            count -= 1
+
+    # Declares wether the player won or lost the game and lets them retry again
+    if win_condition == True:
+        print("You won the game, congratulations!!!")
+        cont = input("Would you like to try again? yes/no ")
+        if cont.lower() == "yes":
+            continue
+        else:
+            game_loop = False
     else:
-        print("Please enter a valid number")
+        print("Sorry, you lost.")
+        cont = input("Would you like to try again? yes/no ")
 
-if win_condition == True:
-    print("You won the game, congratulations!!!")
-else:
-    print("Sorry, you lost. Try again sometime!")
+        if cont.lower() == "yes":
+            continue
+        else:
+            game_loop = False
 
-
+print("Hope you'll play again someday!")
